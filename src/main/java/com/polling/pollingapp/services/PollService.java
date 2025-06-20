fce279d3ffe9dc987b3b0927f413c405d71ca110
Youkwhan <Youkwhan@gmail.com>
2025-06-19 22:59:09 -0400

    Add API endpoints to retrieve polls

--- src/main/java/com/polling/pollingapp/services/PollService.java
+++ src/main/java/com/polling/pollingapp/services/PollService.java
@@ -2,7 +2,11 @@ package com.polling.pollingapp.services;
 
 import com.polling.pollingapp.model.Poll;
 import com.polling.pollingapp.repositories.PollRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PollService {
@@ -14,5 +18,14 @@ public class PollService {
 
     public Poll createPoll(Poll poll) {
         return pollRepository.save(poll);
+    }
+
+    public List<Poll> getAllPolls() {
+        return pollRepository.findAll();
+    }
+
+    // optional because findbyid returns a optional
+    public Optional<Poll> getPollById(Long id) {
+        return pollRepository.findById(id);
     }
 }
