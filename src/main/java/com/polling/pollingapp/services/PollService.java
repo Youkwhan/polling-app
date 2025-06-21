b819e78cf822c79ae787d6c0a0b8f6c93b962c49
Youkwhan <Youkwhan@gmail.com>
2025-06-21 18:35:36 -0400

    Add voting functionality for polls

--- src/main/java/com/polling/pollingapp/services/PollService.java
+++ src/main/java/com/polling/pollingapp/services/PollService.java
@@ -1,10 +1,13 @@
 package com.polling.pollingapp.services;
 
+import com.polling.pollingapp.dto.Vote;
+import com.polling.pollingapp.model.OptionVote;
 import com.polling.pollingapp.model.Poll;
 import com.polling.pollingapp.repositories.PollRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,5 +30,23 @@ public class PollService {
     // optional because findbyid returns a optional
     public Optional<Poll> getPollById(Long id) {
         return pollRepository.findById(id);
+    }
+
+    public void vote(Long pollId, int optionIndex) {
+        // get poll from db (repository)
+        // get options
+        // increment voteCount from option.
+        Poll poll = pollRepository.findById(pollId)
+                .orElseThrow(() -> new RuntimeException("Poll not found"));
+
+        List<OptionVote> options = poll.getOptions();
+        if (optionIndex < 0 || optionIndex >= options.size()) {
+            throw new IllegalArgumentException("Invalid option index");
+        }
+
+        OptionVote selectedOption = options.get(optionIndex);
+        selectedOption.setVoteCount(selectedOption.getVoteCount() + 1);
+
+        pollRepository.save(poll);
     }
 }
