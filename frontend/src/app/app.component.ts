922896818c8b5592ccf940440d7330de066d4b86
Youkwhan <Youkwhan@gmail.com>
2025-06-21 20:42:15 -0400

    refactor: restructure app components and remove unused files

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PollComponent } from './poll/poll.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [PollComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class App {
   protected title = 'poll-app';
